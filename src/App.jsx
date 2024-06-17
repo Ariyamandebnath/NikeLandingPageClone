293d1a446260324dcfe0b401ec49e6509bbba475
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-06-17 11:51:33 +0530

    nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Nav from "./components/nav";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div>hello This is Ariyaman</div>
+    <div>
+      <Nav></Nav>
+    </div>
   )
 }
 
---
+++ src/components/nav.jsx
@@ -0,0 +1,20 @@
+const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <div className="logo">
+                    <img src="../public/images/logo.png" alt="logo" />
+                </div>
+                <ul>
+                    <li href="#">Menu</li>
+                    <li href="#">Location</li>
+                    <li href="#">About</li>
+                    <li href="#">Contact</li>
+                </ul>
+                <button>login</button>
+            </nav>
+        </div>
+    );
+};
+
+export default Nav;
